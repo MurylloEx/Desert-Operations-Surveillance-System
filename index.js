435fa1c16637ef82b5cf43403c0658d55c39e1bf
Luiz <gugamatias7@gmail.com>
2020-03-22 12:47:25 -0300

    Alterações Gustavo

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
-var murylloÉgay = true;
-while(murylloÉgay == true){console.log("Me responde viado!!!!")}
+// Importações 
+const express = require("express");
+const mysql = require("mysql");
+const app = express();
+const path = require('path');
+const colors = require('colors');
+const serveIndex = require("serve-index");
+
+app.use((req, res, next)=>{
+    console.log("-->> -----");
+    console.log("-->> -----");
+    console.log("-->> ------");
+    console.log(req.body);  
+    next();
+});
+
+
+app.use("/", serveIndex(path.join(__dirname, "public"), {icons: true}));
+app.use("/", express.static(path.join(__dirname, "public")));
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.listen(80, ()=>{
+    console.log("O server está rodando...")
+});
